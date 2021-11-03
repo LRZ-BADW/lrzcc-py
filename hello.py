dac9bcedf90b53bdc84b2df539b716d20898be33
Sandro-Alessio Gierens <sandro-alessio@gierens.de>
2021-11-03 01:10:15 +0100

    fixed formatting in hello module according to flake8

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from argparse import _SubParsersAction, ArgumentParser, Namespace
 import requests
 import tabulate
-import json
 
 
 cmds_with_sub_cmds = ['hello']
+
+
+def do_nothing(variable):
+    pass
 
 
 def setup_parsers(main_subparsers: _SubParsersAction):
@@ -14,28 +17,32 @@ def setup_parsers(main_subparsers: _SubParsersAction):
     # hello action parser
     hello_parser: ArgumentParser = main_subparsers.add_parser(
         "hello",
-        help = "hello commands",
+        help="hello commands",
         )
     parsers['hello'] = hello_parser
     hello_subparsers: _SubParsersAction = \
-            hello_parser.add_subparsers(
-                    help="sub-commands",
-                    dest="sub_command",
-                    )
+        hello_parser.add_subparsers(
+                help="sub-commands",
+                dest="sub_command",
+                )
 
     # hello user parser
     hello_user_parser: ArgumentParser = \
-            hello_subparsers.add_parser(
-                    "user",
-                    help = "Hello as normal user",
-                    )
+        hello_subparsers.add_parser(
+                "user",
+                help="Hello as normal user",
+                )
 
     # hello admin parser
     hello_admin_parser: ArgumentParser = \
-            hello_subparsers.add_parser(
-                    "admin",
-                    help = "Hello as admin user",
-                    )
+        hello_subparsers.add_parser(
+                "admin",
+                help="Hello as admin user",
+                )
+
+    # just to avoid unused variable warnings
+    do_nothing(hello_user_parser)
+    do_nothing(hello_admin_parser)
 
     return parsers
 
